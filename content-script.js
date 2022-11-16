77322f01532846607de80576ad120615bb7b5baf
Karl Wolf <karl.wolf2706@gmail.com>
2022-11-16 15:34:45 +0100

    inserting html only when time changes

--- content-script.js
+++ content-script.js
@@ -37,7 +37,7 @@ loadContentWith = (dates) => {
 }
 
 calculateWork = (value) => {
-    var hoursAndMinutes = item.trim().split("h");
+    var hoursAndMinutes = value.trim().split("h");
     var duration = hoursAndMinutes[0] * 3600000 + hoursAndMinutes[1] * 60000;
     return { "work": duration, "break": 0 };
 }
@@ -84,6 +84,60 @@ mapMilliSecondsToHoursAndMinutes = (sum) => {
     return { "hours": hours, "minutes": minutes, "seconds": seconds };
 }
 
+insertIntoPage = (workTime) => {
+    $(".AttendancePage-module__header___cbl1Z").find("#myAttendance").remove();
+    $(".AttendancePage-module__header___cbl1Z").append(`
+    <div id="myAttendance" class="WidgetArea-module__widgetsWrapper___TVJCo" style="margin-top: 20px;">
+    <div class="Widget-module__wrapper___xmbJe">
+        <div class="WidgetHead-module__name___A2zdl">Arbeitszeitkonto</div>
+        <div class="WidgetBody-module__wrapper___dqSWy">
+            <div class="AttendanceBalanceWidget-module__left___XChX0">
+                <div class="WidgetItem-module__wrapper___zgWan">
+                    <div class="WidgetItemTitle-module__title___JvxYv"><span>53h10</span></div>
+                    <div class="WidgetItemSubtitle-module__subtitle___LLkNS"><span>Erfasst</span></div>
+                </div>
+                <div class="WidgetItem-module__wrapper___zgWan">
+                    <div class="WidgetItemTitle-module__title___JvxYv"><span>53h10</span></div>
+                    <div class="WidgetItemSubtitle-module__subtitle___LLkNS"><span>Erfasst</span></div>
+                </div>
+                <div class="WidgetItem-module__wrapper___zgWan">
+                    <div class="WidgetItemTitle-module__title___JvxYv"><span>53h10</span></div>
+                    <div class="WidgetItemSubtitle-module__subtitle___LLkNS"><span>Erfasst</span></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div data-test-id="widget-work-schedule-container" class="Widget-module__wrapper___xmbJe">
+        <div class="WidgetHead-module__name___A2zdl">Arbeitstage &amp; Wochenstunden</div>
+        <div class="WidgetBody-module__wrapper___dqSWy">
+            <div class="WorkScheduleWidget-module__left____3U8L">
+                <div class="WidgetItem-module__wrapper___zgWan">
+                    <div class="WidgetItemTitle-module__title___JvxYv"><span data-test-id="widget-work-schedule-days">5
+                            Tage</span></div>
+                    <div class="WidgetItemSubtitle-module__subtitle___LLkNS"><span
+                            data-test-id="widget-work-schedule-weekdays">Mo, Di, Mi, Do, Fr</span></div>
+                </div>
+                <div class="WidgetItem-module__wrapper___zgWan">
+                    <div class="WidgetItemTitle-module__title___JvxYv"><span
+                            data-test-id="widget-work-schedule-hours">20h</span></div>
+                    <div class="WidgetItemSubtitle-module__subtitle___LLkNS"><span>Wochenstunden</span></div>
+                </div>
+            </div>
+            <div class="WorkScheduleWidget-module__ellipsis___bBt7v">
+                <div class="DropdownMenu-module__2p2rUa8Y__v3-0-17">
+                    <div data-test-id="widget-work-schedule-menu" data-action-name="widget-work-schedule-menu"
+                        class="DropdownMenuToggle-module__3kJdGE4x__v3-0-17"><i class="far fa-ellipsis-h"
+                            style="font-size: 16px; color: var(--icon-subdued);"></i></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>`);
+}
+
+var prevWorkingTime;
+var prevBreakTime;
+
 doSomething = () => {
     var dateArray = getDatesInWeek(new Date());
 
@@ -96,18 +150,17 @@ doSomething = () => {
     var workingTime = mapMilliSecondsToHoursAndMinutes(totals.work);
     var breakTime = mapMilliSecondsToHoursAndMinutes(totals.break);
 
-    console.log("Worked this week for: ");
-    console.log(workingTime);
-    console.log("Break this week for: ");
-    console.log(breakTime);
+    if (prevWorkingTime && (prevWorkingTime.hours === workingTime.hours && prevWorkingTime.minuntes === workingTime.minutes))
+        return;
 
-    alert("test");
+    if (prevBreakTime && (prevBreakTime.hours === breakTime.hours && prevBreakTime.minuntes === breakTime.minutes))
+        return;
+
+    prevWorkingTime = workingTime;
+    prevBreakTime = breakTime;
+
+    insertIntoPage(workingTime);
 }
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if(request.action == "start"){
-        document.addEventListener('DOMContentLoaded', doSomething, false);
-    }
-});
-
+setInterval(doSomething, 1000);
 
